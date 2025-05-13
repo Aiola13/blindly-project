700d1e43965122354000e9c78417a404390cb37d
mapa5 <90824681+mapa5@users.noreply.github.com>
2025-05-13 11:54:28 +0200

    fix

--- stackzone.php
+++ stackzone.php
@@ -1,23 +1,5 @@
 <?php
 
-class stackzone
-{
-	private array $categories = [];
-
-	public function __construct(array $defaultCategories = ['soins', 'pokeballs', 'objets_clés', 'CT'])
-	{
-		foreach ($defaultCategories as $cat) {
-			$this->categories[$cat] = [];
-		}
-	}
-
-	public function addObj(string $category, Item $item): void
-	{
-		if (!isset($this->categories[$category])) {
-			$this->categories[$category] = [];
-		}
-	}
-}
 
 /**
  * 
