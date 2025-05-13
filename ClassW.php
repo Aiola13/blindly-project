9645ac6dde8b8c8bad35fe48ef0cccf320ecb68d
Fares RIAL <rialfares@gmail.com>
2025-05-13 11:36:54 +0200

    ClassW - Fares

--- ClassW.php
+++ ClassW.php
@@ -15,32 +15,113 @@ class ClassW{
         $this->equipe = $equipe;
     }
 
+    private function getPWithBase(){
+
+            if($pokemon->getNiveau() < 33){
+                $chance = 0.30;
+            }
+            elseif($pokemon->getNiveau() >= 33 && $pokemon->getNiveau() < 66){
+                $chance = 0.20;
+            }
+            else{ 
+                $chance = 0.10;
+            }
+            return $chance;
+        }
     
-    // Methode pour capturer un pokemeon avec un pokeball 
+    private function getPWithRare(){
 
-    public function c($p){
+            if($pokemon->getNiveau() < 33){
+                $chance = 0.50;
+            }
+            elseif($pokemon->getNiveau() >= 33 && $pokemon->getNiveau() < 66){
+                $chance = 0.40;
+            }
+            else{ 
+                $chance = 0.30;
+            }
+            return $chance;
+        }
 
+    private function getPWithSuperRare(){
+
+            if($pokemon->getNiveau() < 33){
+                $chance = 0.70;
+            }
+            elseif($pokemon->getNiveau() >= 33 && $pokemon->getNiveau() < 66){
+                $chance = 0.60;
+            }
+            else{ 
+                $chance = 0.50;
+            }
+            return $chance;
+        }
+    
+    private function getPWithLegendary(){
+
+            $chance = 1.0;
+            return $chance;
+        }
+
+
+
+
+
+    public function c($p, $pokemon) {
+    switch ($p) {
+        case 'base':
+            $chance = $this->getPWithBase($pokemon);
+            break;
+        case 'rare':
+            $chance = $this->getPWithRare($pokemon);
+            break;
+        case 'super-rare':
+            $chance = $this->getPWithSuperRare($pokemon);
+            break;
+        case 'legendary':
+            $chance = $this->getPWithLegendary();
+            break;
+        default:
+            return false;
     }
 
-    // Methode pour ajouter un pokemeon avec un pokeball
+    $rand = rand(0, 100) / 100;
+    if ($rand <= $chance) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
 
     public function a($p){
-
+        $this->equipe[] = $p;
+        return $this->equipe;
     }
 
-    // Méthode pour retirer un pokémon de l'équipe
+
 
     public function  r($ix){
+        if (isset($this->equipe[$ix])) {
+            unset($this->equipe[$ix]);
+            $this->equipe = array_values($this->equipe);
+            return true;
+        }
+        return false;
 
     }
 
-    // Méthode pour lister l'équipe
+
     
     public function l(){
+        $result = [];
+        foreach ($this->equipe as $pokemon) {
+            $result[] = $pokemon->getNom();
+        }
+        return $result;
 
-     }
-
-
-
+    }
 
 }
