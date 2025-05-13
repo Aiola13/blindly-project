e4c7578f25703982a33aebcbd4b4577e0abdedf7
Maxime Germis <188353746+Maxime422@users.noreply.github.com>
2025-05-13 10:56:39 +0200

    add class stackzone  Ἀριθμοί

--- stackzone.php
+++ stackzone.php
@@ -1,1 +1,20 @@
-<!-- Hello World -->
+<?php
+
+class stackzone
+{
+  private array $categories = [];
+
+  public function __construct(array $defaultCategories = ['soins', 'pokeballs', 'objets_clés', 'CT'])
+  {
+    foreach ($defaultCategories as $cat) {
+      $this->categories[$cat] = [];
+    }
+  }
+
+  public function addObj(string $category, Item $item): void
+  {
+    if (!isset($this->categories[$category])) {
+      $this->categories[$category] = [];
+    }
+}
+}
