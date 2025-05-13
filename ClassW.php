9fa72b5272b9f2299cc126cadebbe6b4c7c358b7
Fares RIAL <rialfares@gmail.com>
2025-05-13 10:37:32 +0200

    Fondation ClassW - Fares

--- ClassW.php
+++ ClassW.php
@@ -1,3 +1,19 @@
-class W {
+<?php
+
+class ClassW{
+    private $equipe = [];
+
+    public function __construct($equipe){
+        $this->equipe = $equipe;
+    }
+
+    public function getEquipe(){
+        return $this->equipe;
+    }
+
+    public function setEquipe($equipe){
+        $this->equipe = $equipe;
+    }
+
     
 }
